a5b76745f8852a8f02c0ba65ffcd24a067a599bd
shader-boy <shader_boy@hotmail.com>
2024-04-21 23:25:09 +0100

    Linked List first version

--- test_script.py
+++ test_script.py
@@ -1,10 +1,30 @@
-def greet_person(name):
-    print(f"Hello dear ${name}")
+class Node:
+    # Creating a node
+    def __init__(self, item):
+        self.item = item
+        self.next = None
 
-def sum_nums(a, b):
-    return a + b
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
 
 
+if __name__ == '__main__':
 
-greet_person('Thomas')
-sum_nums(4, 6)
+    linked_list = LinkedList()
+
+    # Assign item values
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    # connect nodes
+    linked_list.head.next = second
+    second.next = third
+
+    # Print the linked list item
+    while linked_list.head != None:
+        print(linked_list.head.item, end=' ')
+        linked_list.head = linked_list.head.next
